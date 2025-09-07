e323d800fe0088cf2e6f2e34771fef9cb8a061f0
scaleoutSean <scaleoutsean@users.noreply.github.com>
2025-09-07 22:58:39 +0800

    Add script to download MCP and Grafana source code

---
+++ scripts/fetch-external.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# 
+# download external repository data
+# 
+#
+git clone https://github.com/influxdata/influxdb3_mcp_server influx-mcp 
+
+git clone https://github.com/grafana/grafana 
