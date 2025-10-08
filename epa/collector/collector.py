50f1a097b182857cf2ea0b2da42fdfa4889de79c
scaleoutSean <scaleoutsean@users.noreply.github.com>
2025-10-08 19:45:31 +0800

    Update version strings in .env and collector to 3.5.2

--- epa/collector/collector.py
+++ epa/collector/collector.py
@@ -47,7 +47,7 @@ INFLUXDB_PORT = 8086
 INFLUXDB_DATABASE = 'eseries'
 DEFAULT_RETENTION = '52w'  # 1y
 
-__version__ = '3.5.1'
+__version__ = '3.5.2'
 
 #######################
 # LIST OF METRICS #####
